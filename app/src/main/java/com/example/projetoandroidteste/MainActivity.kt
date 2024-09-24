4773e388a9e72d7d3c4f309efd025b65efb5fa1a
Erick de Oliveira Caprini <292176@mesquita.com.br>
2024-09-24 15:19:51 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetoandroidteste/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.projetoandroidteste
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projetoandroidteste.ui.theme.ProjetoAndroidTesteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjetoAndroidTesteTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Meu projeto Adroid!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProjetoAndroidTesteTheme {
+        Greeting("Android")
+    }
+}
